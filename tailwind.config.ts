ec4fb99361a0aab82376339d8d23c4122f20d456
Saif <saif49032@gmail.com>
2023-11-30 18:27:22 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+ const defaultTheme=require('tailwindcss/defaultTheme')
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
@@ -6,13 +7,20 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  theme: { 
+    extend: { 
+      backgroundImage:{
+        "main-bg": "url('/bg-img.jpeg')",
       },
+     fontFamily:{
+      bodyFont:['Urbanist',...defaultTheme.fontFamily.sans ],  
+     },
+     colors:{
+      bodyColor:"#fbfaf7",
+      bgLight:"#1010100d",
+      darkText:"#242424",
+      lightText:"#a5a5a5",
+     },
     },
   },
   plugins: [],
