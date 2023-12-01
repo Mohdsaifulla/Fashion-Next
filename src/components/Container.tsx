5e5348fecd63a787c2c5d5ebdccaba2053f72199
Saif <saif49032@gmail.com>
2023-12-01 17:56:11 +0530

    Header

---
+++ src/components/Container.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+interface Props {
+  children: React.ReactNode;
+  className?: String;
+}
+
+const Container = ({ children, className }: Props) => {
+  return (
+    <div className={`${className} mx-auto px-4 xl:px-0 py-10`}>{children}</div>
+  );
+};
+
+export default Container;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
-
+import { hkdf } from "crypto";
+import Container from "./Container";
+import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="bg-bodyColor h-20 top-0 sticky z-50">
+      <Container className="h-full flex items-center md:gap-x-5 justify-between md:justify-start">
+        <Logo />
+      </Container>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
